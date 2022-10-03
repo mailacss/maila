8808981b9abedc9c700f20430b2c95325ace713b
Maila Cardoso <cardmaila19@gmail.com>
2022-10-03 13:03:50 -0300

    Cria select

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,18 @@
 var button = document.getElementById('button');
+
+
+function habilita(){
+
+    var option = document.getElementById("option").value;
+ 
+    if(option !="0" ){ 
+        document.getElementById("field1").disabled = false;
+        document.getElementById("field2").disabled = false;
+        document.getElementById("button").disabled = false;
+    }else{
+        document.getElementById("field1").disabled = true;
+        document.getElementById("field2").disabled = true;
+        document.getElementById("button").disabled = true;
+    }
+
+}
